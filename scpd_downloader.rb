cdd8e06aa99b0e68829d9e96e50a773eb4bd0acd
Denny Britz <dennybritz@gmail.com>
2013-10-25 14:19:11 -0700

    revert previous change

--- scpd_downloader.rb
+++ scpd_downloader.rb
@@ -27,7 +27,7 @@ end
 page = agent.get(CURRENT_QUARTER_URL)
 
 # Fill out the login form
-while page.form("login")
+if page.form("login")
   login_form = page.form("login")
   print "SUNet ID: "
   login_form.username = gets.strip
