7fd203373cd503af794f3519499254331d9b3694
Daniel Frank <danfrankj@gmail.com>
2013-12-05 11:08:39 -0800

    added automatic file naming and ability to download all lectures

--- scpd_downloader.rb
+++ scpd_downloader.rb
@@ -9,20 +9,27 @@ require "mechanize"
 require "json"
 require "io/console"
 
-BASE_URL = "https://myvideosu.stanford.edu/"
+BASE_URL = "https://myvideosu.stanford.edu"
 CURRENT_QUARTER_URL = "https://myvideosu.stanford.edu/oce/currentquarter.aspx"
 COURSE = ARGV.shift
-LECTURE_NUM = (ARGV.shift || 1).to_i - 1
-FILENAME = ARGV.shift || "output.mp4"
 
 # Parse command line options
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: scpd_downloader.rb [course] [lecture_num] [filename] [options]"
+  opts.banner = "Usage: scpd_downloader.rb [course] [lecture_num] [options]"
   opts.on("-l", "--link", "Print raw video link instead of downloading the lecture.") do |links|
     options[:link] = links
   end
 end.parse!
+puts options
+LECTURE_NUM = ARGV.shift
+
+if not LECTURE_NUM.nil?
+    LECTURE_NUM = LECTURE_NUM.to_i - 1
+    puts "Downloading lecture #{LECTURE_NUM + 1} for course #{COURSE}"
+else
+    puts "Downloading all lectures for course #{COURSE}"
+end
 
 agent = Mechanize.new
 agent.user_agent_alias = "Mac Safari"
@@ -57,33 +64,55 @@ end
 
 # Save cookies
 agent.cookie_jar.save_as "cookies", session: true, format: :yaml
-
+page = agent.click(page.link_with(:text => BASE_URL))
 # Find the course number and visit the course page
 course_url = page.content  =~ /href=\"(.*#{COURSE}.*?)\"/i && (BASE_URL + $1)
+
+
+def download(link, agent, options)
+    link.href =~ /javascript:openSL\((.*?)\);/
+    coll, course, co, lecture, lectureDesc, authtype, playerType = *($1.gsub("\"","").split(","))
+    fname = "#{course}_#{lecture}.wmv"
+    if File.exist?(fname)
+        puts "File already exists #{fname}"
+        return
+    else 
+        puts "Downloading #{fname}"
+    end
+    # Get the SLP hash for authentication via JSON request
+    json_response = agent.post("https://myvideosu.stanford.edu/OCE/GradCourseInfo.aspx/playSLVideo", 
+                               {coGuidstr: co, collGuidstr: coll, desiredAuthType: "WA"}.to_json, "Content-Type" => "application/json;")
+    slp = JSON(json_response.body)["d"]
+    player_url = "http://myvideosv.stanford.edu/player/slplayer.aspx?coll=#{coll}&course=#{course}&co=#{co}&lecture=#{lecture}&authtype=#{authtype}&slp=#{slp}&sl=true"
+    page = agent.get(player_url)
+
+    # Get the video URL and download using mimms
+    video_url = page.content =~ /(mms:\/\/.*\.wmv)/ && $1
+    video_url.gsub!("mms","mmsh")
+
+
+    if options[:link]
+      puts video_url
+    else
+      system("ffmpeg -i #{video_url} #{fname}")
+    end
+end
+
+
 page = agent.get(course_url)
-
-# Visit the player page for the specified lecture
-lecture_link = page.links.select { |link| link.text == "SL"}.reverse[LECTURE_NUM]
-if lecture_link.nil?
-  $stderr.puts "No such lecture."
-  exit 1
-else
-  lecture_link.href =~ /javascript:openSL\((.*?)\);/ 
-  coll, course, co, lecture, lectureDesc, authtype, playerType = *($1.gsub("\"","").to_s.split(","))
+links = page.links.select { |link| link.text == "SL" }.reverse
+if not LECTURE_NUM.nil?
+    links = [links[LECTURE_NUM]]
 end
 
-# Get the SLP hash for authentication via JSON request
-json_response = agent.post("https://myvideosu.stanford.edu/OCE/GradCourseInfo.aspx/playSLVideo", 
-  {coGuidstr: co, collGuidstr: coll, desiredAuthType: "WA"}.to_json, "Content-Type" => "application/json;")
-slp = JSON(json_response.body)["d"]
-player_url = "http://myvideosv.stanford.edu/player/slplayer.aspx?coll=#{coll}&course=#{course}&co=#{co}&lecture=#{lecture}&authtype=#{authtype}&slp=#{slp}&sl=true"
-page = agent.get(player_url)
-
-# Get the video URL and download using mimms
-video_url = page.content =~ /(mms:\/\/.*\.wmv)/ && $1
-video_url.gsub!("mms","mmsh")
-if options[:link]
-  puts video_url
-else
-  system("ffmpeg -i #{video_url} #{FILENAME}")
+for link in links
+    puts link.href
+    begin 
+        download(link, agent, options)
+    rescue Exception => e
+        puts "Failed to download"
+        puts e
+    end
 end
+
+
